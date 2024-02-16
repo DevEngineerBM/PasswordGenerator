e039ee8ee7c4b2a44dbcc834ecf4b949dcd2b083
belkhalil <belkhalil1mohamed@gmail.com>
2024-02-16 20:56:53 +0100

    few modifications in html and css and finishing JS file

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const passwordInput = document.getElementById('Password');
+const generate = document.getElementById('btn');
+
+function generatePassword() {
+
+    const caracters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+?:{}[]';
+
+    const length = 12;
+    let password = '';
+
+    for ( let i = 0 ; i < length ; i++) {
+
+const randomx = Math.floor(Math.random() * caracters.length)
+
+password += caracters.charAt(randomx)
+
+/* password = password + charset.charAt(randomIndex) */
+    }
+
+passwordInput.value = password;
+
+}
+
+
+ generate.addEventListener('click', generatePassword);
+
